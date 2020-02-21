e823bbc544339dab8309c3950584cbe591b02ddd
mattsse <matthias.seitz@tum.de>
2020-02-21 22:56:39 +0100

    use fully qualified syntax

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ pub mod ratcliff;
 pub mod token;
 mod utils;
 
-pub fn strdistance<S, T, D>(a: S, b: T, dist: D) -> D::Dist
+pub fn strdistance<S, T, D>(a: S, b: T, dist: D) -> <D as Distance>::Dist
 where
     S: AsRef<str>,
     T: AsRef<str>,
@@ -36,21 +36,19 @@ pub trait Distance {
 
 /// Convenience
 pub trait DistanceElement {
-    fn distance<S, D>(&self, other: S, dist: D) -> D::Dist
+    fn distance<S, D>(&self, other: S, dist: &D) -> <D as Distance>::Dist
     where
         S: AsRef<str>,
         D: Distance;
 }
 
 impl<T: AsRef<str>> DistanceElement for T {
-    fn distance<S, D>(&self, other: S, dist: D) -> D::Dist
+    fn distance<S, D>(&self, other: S, dist: &D) -> <D as Distance>::Dist
     where
         S: AsRef<str>,
         D: Distance,
     {
-        let a = self.as_ref();
-
-        unimplemented!()
+        dist.distance(self, other)
     }
 }
 
