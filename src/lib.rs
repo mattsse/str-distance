0b4c8d58888038718cf44c79b06edc222b89cf23
mattsse <matthias.seitz@tum.de>
2020-02-21 22:47:46 +0100

    add PartialOrd as requirement for Dist associtated type

--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,10 @@ pub mod jaro;
 pub mod levenshtein;
 pub mod qgram;
 pub mod ratcliff;
+pub mod token;
 mod utils;
 
-pub fn strdistance<S, T, D>(a: S, b: T, dist: D)
+pub fn strdistance<S, T, D>(a: S, b: T, dist: D) -> D::Dist
 where
     S: AsRef<str>,
     T: AsRef<str>,
@@ -22,38 +23,27 @@ where
 {
     unimplemented!()
 }
-
-pub fn strcompare<S, T, D>(a: S, b: T, dist: D)
-where
-    S: AsRef<str>,
-    T: AsRef<str>,
-    D: Distance,
-{
-    unimplemented!()
-}
-
-// drawbacks from trait: needs instance
 
 pub trait Distance {
-    type Dist;
+    /// Represents the data type in which this distance is evaluated.
+    type Dist: PartialOrd;
+    /// Evaluates the distance between two str.
     fn distance<S, T>(&self, a: S, b: T) -> Self::Dist
     where
         S: AsRef<str>,
         T: AsRef<str>;
 }
 
-pub struct Score(pub f64);
-
 /// Convenience
 pub trait DistanceElement {
-    fn distance<S, D>(&self, other: S, dist: D)
+    fn distance<S, D>(&self, other: S, dist: D) -> D::Dist
     where
         S: AsRef<str>,
         D: Distance;
 }
 
 impl<T: AsRef<str>> DistanceElement for T {
-    fn distance<S, D>(&self, other: S, dist: D)
+    fn distance<S, D>(&self, other: S, dist: D) -> D::Dist
     where
         S: AsRef<str>,
         D: Distance,
@@ -64,7 +54,7 @@ impl<T: AsRef<str>> DistanceElement for T {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd)]
 pub enum DistanceValue {
     Exact(usize),
     Exceeded(usize),
