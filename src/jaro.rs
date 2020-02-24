bc8390d5ffd969c051b32a24e06861b9591d0d04
mattsse <matthias.seitz@tum.de>
2020-02-24 22:29:53 +0100

    fix jaro distance impl

--- src/jaro.rs
+++ src/jaro.rs
@@ -1,45 +1,93 @@
 use crate::utils::{count_eq, order_by_len_asc};
 use crate::DistanceMetric;
-
+use std::cmp;
 pub struct Jaro;
 
 impl DistanceMetric for Jaro {
     type Dist = f64;
-    #[inline]
+
+    fn distance<S, T>(&self, a: S, b: T) -> Self::Dist
+    where
+        S: IntoIterator,
+        T: IntoIterator,
+        <S as IntoIterator>::IntoIter: Clone,
+        <T as IntoIterator>::IntoIter: Clone,
+        <S as IntoIterator>::Item: PartialEq + PartialEq<<T as IntoIterator>::Item>,
+        <T as IntoIterator>::Item: PartialEq,
+    {
+        let s1: Vec<_> = a.into_iter().collect();
+        let s2: Vec<_> = b.into_iter().collect();
+
+        let s1_len = s1.len();
+        let s2_len = s2.len();
+
+        // edge cases
+        if s1_len + s2_len == 0 {
+            return 0.0;
+        } else if cmp::min(s1_len, s2_len) == 0 {
+            return 1.0;
+        } else if s1_len + s2_len == 2 {
+            return if s1[0] == s2[0] { 0. } else { 1. };
+        }
+
+        let max_dist = cmp::max(s1_len, s2_len) / 2 - 1;
+        let mut s1_matches = vec![false; s1_len];
+        let mut s2_matches = vec![false; s2_len];
+        let mut m = 0usize;
+
+        for i in 0..s1_len {
+            let start = cmp::max(0, i as isize - max_dist as isize) as usize;
+            let end = cmp::min(i + max_dist + 1, s2_len);
+            for j in start..end {
+                if !s2_matches[j] && s1[i] == s2[j] {
+                    s1_matches[i] = true;
+                    s2_matches[j] = true;
+                    m += 1;
+                    break;
+                }
+            }
+        }
+        if m == 0 {
+            return 1.;
+        }
+        let mut t = 0.0;
+        let mut k = 0;
+        for i in 0..s1_len {
+            if s1_matches[i] {
+                while !s2_matches[k] {
+                    k += 1;
+                }
+                if s1[i] != s2[k] {
+                    t += 0.5;
+                }
+                k += 1;
+            }
+        }
+        let m = m as f64;
+        1. - (m / s1_len as f64 + m / s2_len as f64 + (m - t) / m) / 3.0
+    }
+
     fn str_distance<S, T>(&self, s1: S, s2: T) -> Self::Dist
     where
         S: AsRef<str>,
         T: AsRef<str>,
     {
         let (s1, s2) = order_by_len_asc(s1.as_ref(), s2.as_ref());
+        self.distance(s1.chars(), s2.chars())
+    }
 
-        let s1_len = s1.chars().count();
-        let s2_len = s2.chars().count();
-
-        // edge cases
-        if s2_len == 0 {
-            // guaranteed s1 <= s2 --> both are empty
-            return 0.0;
-        }
-        if s1_len == 0 {
-            return 1.0;
-        }
-        if s2_len == 1 {
-            return if s1.chars().next().unwrap().eq(&s2.chars().next().unwrap()) {
-                0.0
-            } else {
-                1.0
-            };
-        }
-
-        let max_dist = ((s2_len / 2) - 1).max(0);
-
-        let mut flags = vec![false; s2_len];
-
-        let mut transpositions = 0.0;
-        let mut c2_match_idx = 0;
-
-        let mut matches = 0.0;
+    fn normalized<S, T>(&self, a: S, b: T) -> f64
+    where
+        S: IntoIterator,
+        T: IntoIterator,
+        <S as IntoIterator>::IntoIter: Clone,
+        <T as IntoIterator>::IntoIter: Clone,
+        <S as IntoIterator>::Item: PartialEq + PartialEq<<T as IntoIterator>::Item>,
+        <T as IntoIterator>::Item: PartialEq,
+    {
+        self.distance(a, b)
+    }
+}
 
         for (s1_idx, c1) in s1.chars().enumerate() {
             let min_bound =
